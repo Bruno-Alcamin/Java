b1d0e064a008ddee86a5ac9b53605aee3e01024c
Bruno Alcamin Moreira <bruno_alcamin@hotmail.com>
2015-06-30 12:38:20 -0300

    Java ED

---
+++ ListaTres/src/Exercicio7.java
@@ -0,0 +1,14 @@
+
+public class Exercicio7 {
+
+	public static void main(String[] args) {
+		int x=2,y=0;
+		
+		while (x<=100) {
+			y=y+x;
+			x+=2;
+		}
+		System.out.println("O valor da soma dos números pares é: "+y);
+	}
+
+}
